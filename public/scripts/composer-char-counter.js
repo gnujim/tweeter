e70197e5c015f9473571c15e185c8a6241442913
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-09-19 11:26:14 -0700

    counter count turns red when below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,15 @@
 // CHARACTER COUNTER
 
 $(document).ready(function() {
-  $('textarea').on('keyup', function() {
+  $('textarea').on('keydown', function() {
     var textAmount = $(this).val().length;
     var remaining = 140 - textAmount;
-    $(this)
-      .siblings('.counter')
-      .text(remaining);
+    var counter = $(this).siblings('.counter');
+    counter.text(remaining);
+    if (remaining < 0) {
+      counter.addClass('negativeCount');
+    } else {
+      counter.removeClass('negativeCount');
+    }
   });
 });
