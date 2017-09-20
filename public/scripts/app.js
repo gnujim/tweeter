e7608160218711bc18fe303fbec720c55027d1c6
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-09-20 14:50:36 -0700

    implemented slide toggle of compose tweet form, added compose button to nav bar (received code review here)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,58 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-function renderTweets(tweets) {
-  $('#tweets-container').empty();
-  tweets.forEach(i => {
-    var tweet = createTweetElement(i);
-    $(function() {
-      $(tweet).appendTo('#tweets-container');
-    });
-  });
-}
-
-function createTweetElement(tweet) {
-  return `
-  <article class="tweet">
-  <header>
-    <a href="#">
-      <img class="user-avatar" src="${tweet.user.avatars.small}"/>
-    </a>
-    <h2 class="user-name">${tweet.user.name}</h2>
-    <a class="user-handle" href="#">${tweet.user.handle}</a>
-  </header>
-  <p class="tweet-content">${tweet.content.text}</p>
-  <footer>
-    <div class="timestamp">${moment(tweet.created_at).fromNow()}</div>
-    <div class="icons">
-      <a href="#">
-        <i class="fa fa-flag" aria-hidden="true"></i>
-      </a>
-      <a href="#">
-        <i class="fa fa-retweet" aria-hidden="true"></i>
-      </a>
-      <a href="#">
-        <i class="fa fa-heart" aria-hidden="true"></i>
-      </a>
-    </div>
-  </footer>
-  </article>`;
-}
-
-function loadTweets() {
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-    success: function(data) {
-      renderTweets(data);
-    }
-  });
-}
-
 $(function() {
-  var $form = $('.new-tweet form');
+  function renderTweets(tweets) {
+    $('#tweets-container').empty();
+    tweets.forEach(i => {
+      var tweet = createTweetElement(i);
+      $(function() {
+        $(tweet).prependTo('#tweets-container');
+      });
+    });
+  }
 
-  $form.on('submit', function(event) {
+  function createTweetElement(tweet) {
+    return `
+          <article class="tweet">
+          <header>
+            <a href="#">
+              <img class="user-avatar" src="${tweet.user.avatars.small}"/>
+            </a>
+            <h2 class="user-name">${tweet.user.name}</h2>
+            <a class="user-handle" href="#">${tweet.user.handle}</a>
+          </header>
+          <p class="tweet-content">${tweet.content.text}</p>
+          <footer>
+            <div class="timestamp">${moment(tweet.created_at).fromNow()}</div>
+            <div class="icons">
+              <a href="#">
+                <i class="fa fa-flag" aria-hidden="true"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-retweet" aria-hidden="true"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-heart" aria-hidden="true"></i>
+              </a>
+            </div>
+          </footer>
+          </article>`;
+  }
+
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function(data) {
+        renderTweets(data);
+      }
+    });
+  }
+
+  $('.new-tweet form').on('submit', function(event) {
     event.preventDefault();
     var $text = $('.tweet-text');
     var tweetText = $text.val();
@@ -73,13 +71,17 @@ $(function() {
       method: 'POST',
       data: content,
       success: function() {
-        $('.tweet-text')
-          .val('')
-          .empty();
-        $('.counter').text('140');
+        $('.tweet-text').val('');
         loadTweets();
       }
     });
   });
+
+  $('.compose').on('click', function() {
+    $('.new-tweet').slideToggle(300, function() {
+      $('.new-tweet textarea').select();
+    });
+  });
+
   loadTweets();
 });
